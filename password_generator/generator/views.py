5b2d8314fa9801be4dfc6bfad7c39d82cb6b2a05
kazila <mrkazzila@gmail.com>
2023-02-07 22:08:59 +0300

    feat: add generating password method

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.views.generic.base import TemplateView
 
 from common.views import TitleMixin
@@ -11,3 +13,26 @@ class HomeView(TitleMixin, TemplateView):
 class PasswordView(TitleMixin, TemplateView):
     template_name = 'generator/password.html'
     title = 'SPG | You password'
+
+    def random_generator(self):
+        base_characters = list('abcdefghijklmnopqrstuvwxyz')
+        data = self.request.GET
+
+        length = int(data.get('length'))
+        if data.get('uppercase'):
+            base_characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+        if data.get('numbers'):
+            base_characters.extend(list('1234567890'))
+        if data.get('special'):
+            base_characters.extend(list('!@#$%^&*()-_=+'))
+
+        password = ''
+        for _ in range(length):
+            password += random.choice(base_characters)
+
+        return password
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['password'] = self.random_generator()
+        return context
