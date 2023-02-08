de192fa5865f964a0cf70c7acf81592e075fd44f
kazila <mrkazzila@gmail.com>
2023-02-08 20:38:23 +0300

    refactor: remove password generator func from views

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,7 +1,7 @@
-import random
+
 
 from django.views.generic.base import TemplateView
-
+from generator.password_generator import random_generator
 from common.views import TitleMixin
 
 
@@ -14,27 +14,9 @@ class PasswordView(TitleMixin, TemplateView):
     template_name = 'generator/password.html'
     title = 'SPG | You password'
 
-    def random_generator(self):
-        base_characters = list('abcdefghijklmnopqrstuvwxyz')
-        data = self.request.GET
-
-        length = int(data.get('length'))
-        if data.get('uppercase'):
-            base_characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
-        if data.get('numbers'):
-            base_characters.extend(list('1234567890'))
-        if data.get('special'):
-            base_characters.extend(list('!@#$%^&*()-_=+'))
-
-        password = ''
-        for _ in range(length):
-            password += random.choice(base_characters)
-
-        return password
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['password'] = self.random_generator()
+        context['password'] = random_generator(request_data=self.request.GET)
         return context
 
 
