890f00603c2f9d02cc57adc9105b0e0618d97bb5
kazila <mrkazzila@gmail.com>
2023-02-23 20:38:38 +0300

    feat: update urls

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -1,24 +1,7 @@
-"""password_generator URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 
 from generator import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('password/', views.PasswordView.as_view(), name='password'),
-    path('about/', views.AboutView.as_view(), name='about'),
+    path('', views.home_view, name='home'),
 ]
