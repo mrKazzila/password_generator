7348e6132f6bd3b0e7bd97ead4f198c4d3c4cfd4
Ilya Kazakov <mrkazzila@gmail.com>
2023-02-23 22:53:11 +0300

    feat: update setting for prod

--- password_generator/password_generator/settings.py
+++ password_generator/password_generator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#$b(5v9k9bi4_roi+(o&dyvabui!7&6zhb@!fgymw*2(n1-nw3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
